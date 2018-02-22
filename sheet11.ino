f6b5fb3d87f2828b4ec79d68cf29f0a164b78b9f
hagarkhaledsaid <36450102+hagarkhaledsaid@users.noreply.github.com>
2018-02-22 19:27:00 +0200

    Add files via upload

---
+++ sheet11.ino
@@ -0,0 +1,19 @@
+ int LED=1;
+
+ 
+ 
+ 
+ void setup() {
+  // put your setup code here, to run once:
+
+
+pinMode(LED,OUTPUT);
+ }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (LED,HIGH);
+delay (500);
+digitalWrite (LED,LOW);
+delay(1500);
+}
---
+++ traficlights.ino
@@ -0,0 +1,27 @@
+int greenled=0;
+int yellowled=8;
+int redled=12;
+
+void setup()
+{
+  pinMode(greenled, OUTPUT);
+  pinMode (yellowled, OUTPUT);
+  pinMode (redled,OUTPUT);
+ 
+}
+
+void loop()
+{ 
+  digitalWrite (greenled,HIGH);
+  delay (7000);
+  digitalWrite (greenled,LOW );
+  digitalWrite( yellowled,HIGH);
+  delay (5000);
+  digitalWrite(yellowled,LOW);
+  digitalWrite (redled,HIGH);
+  delay(9000);
+  digitalWrite (redled,LOW);
+  digitalWrite (yellowled,LOW);
+ 
+}
+
